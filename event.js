c764a619851c300ad2fa86b2a2e241d83bbb89d2
qwertyuio455 <trinhdieulinh000@gmail.com>
2025-03-02 20:22:40 +0700

    dl 3-2

---
+++ event.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) slide.classList.add("active");
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Tự động chuyển slide sau 7 giây
+setInterval(nextSlide, 7000);
+
+// Hiển thị slide đầu tiên
+showSlide(currentSlide);
+
+
+
